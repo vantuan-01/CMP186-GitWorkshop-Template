5c330bb16da3f8c2719c5c06bc5948f6501fe9a0
vantuan-01 <phamanhtuan01789@gmail.com>
2022-03-03 08:22:34 +0700

    task-02-Tuan

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -8,8 +8,12 @@ namespace SalaryCalculator
         {
             var engineerSalary = EmployeeService.CalculateSalary(EmployeeType.Engineer, 2);
             Console.WriteLine(string.Format("Engineer's Salary: {0}", engineerSalary));
+
             var ManagerSalary = EmployeeService.CalculateSalary(EmployeeType.Manager, 7);
             Console.WriteLine(string.Format("Manager's Salary: {0}", ManagerSalary));
+
+            var Expert = EmployeeService.CalculateSalary(Expert.Manager, 9);
+            Console.WriteLine(string.Format("Expert's Salary: {0}", ExpertSalary));
 
             Console.ReadLine();
         }
